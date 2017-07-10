fb7458ff6fd9257a955a191e2af8324565197a79
1150450363kym <1150450363@qq.com>
2017-07-10 17:13:41 +0800

    Initial commit

---
+++ app/src/main/java/comuxi/example/administrator/apps/MainActivity.java
@@ -0,0 +1,13 @@
+package comuxi.example.administrator.apps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
