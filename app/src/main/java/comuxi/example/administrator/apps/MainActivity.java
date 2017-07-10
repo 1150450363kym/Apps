dd66641b357115990f391a09a08169d6c5d79338
799821802 <799821802@qq.com>
2017-07-10 21:05:08 +0800

    abcd

--- app/src/main/java/comuxi/example/administrator/apps/MainActivity.java
+++ app/src/main/java/comuxi/example/administrator/apps/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends Activity {
 //很防i空和飞机哦好吧
     //sss
 //    我在这里在改一次
+//    12345
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
